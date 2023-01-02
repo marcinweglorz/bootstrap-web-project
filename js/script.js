a857af15d2592140ea1a301dd6b1fcee07ef907d
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-01-02 23:02:26 +0100

    finished about us section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
 	const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
+	const navList = document.querySelector('.navbar-collapse')
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
@@ -11,12 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
-    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-
-
-
-
-
+	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
 
 	window.addEventListener('scroll', addShadow)
 })
